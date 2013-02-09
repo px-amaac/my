e7a0bb4e933bfb52cf96145d3e38502d2d2c8664
DoubleAA <px.amaac@gmail.com>
2013-02-09 01:08:49 -0800

    deploying1

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   	before_save { |user| user.email = email.downcase }
-	validates :password, presence: true, length: { minimum: 6 }
+	validates :password, length: { minimum: 6 }
 	validates :password_confirmation, presence: true
   	validates :name, presence: true, length: { maximum:50 }
   	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
