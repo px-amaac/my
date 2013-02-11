9c0dee54c410ca0cf3cdc60b64e2ea72ebbcea4d
DoubleAA <px.amaac@gmail.com>
2013-02-11 13:57:39 -0800

    end chapter 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
   	before_save { |user| user.email = email.downcase }
   	before_save :create_remember_token
 	validates :password, length: { minimum: 6 }
@@ -20,6 +21,10 @@ class User < ActiveRecord::Base
   	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
  	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
     private
 
